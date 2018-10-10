32643154731d033f3fae892a658f1def2ba89cda
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-10-09 23:38:57 -0400

    available gpu option for train.sh

--- research/object_detection/pneumonia/train.sh
+++ research/object_detection/pneumonia/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <data-dir> <config-file> <results-dir>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <data-dir> <config-file> <results-dir> <available-gpus>"
     exit 1
 fi
-docker run --runtime=nvidia --rm -v `pwd`/"$1":/data -v `pwd`/`dirname $2`:/model -v `pwd`/"$3":/results yukw777/tf-models python research/object_detection/model_main.py --pipeline_config_path=/model/`basename $2` --model_dir=/results
+docker run -e "NVIDIA_VISIBLE_DEVICES=$4" --runtime=nvidia --rm -v `pwd`/"$1":/data -v `pwd`/`dirname $2`:/model -v `pwd`/"$3":/results yukw777/tf-models python research/object_detection/model_main.py --pipeline_config_path=/model/`basename $2` --model_dir=/results
