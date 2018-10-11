a647820f390cff87da5da42aac06cd192cc8bf2c
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-10-10 20:01:35 -0400

    script to run tensorboard inside a docker container

---
+++ research/object_detection/pneumonia/tensorboard.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <logdir>"
+    exit 1
+fi
+docker run --runtime=nvidia --rm -p 6006:6006 -v `pwd`/"$1":/logdir yukw777/tf-models tensorboard --logdir /logdir
