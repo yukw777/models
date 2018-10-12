fc86693d0ed2b9e5b408694d397cc7d8efb3e060
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-10-12 19:49:05 -0400

    model_eval.py now saves images with detected boxes and ground truth boxes

--- research/object_detection/inference/model_eval.py
+++ research/object_detection/inference/model_eval.py
@@ -16,7 +16,7 @@ r"""Infers detections on a TFRecord of TFExamples given an inference graph.
 
 Example usage:
   ./infer_detections \
-    --input_tfrecord_paths=/path/to/input/tfrecord1,/path/to/input/tfrecord2 \
+    --input_tfrecord_pattern=/path/to/input/tfrecord1* \
     --output_images_dir=/path/to/output/detections/images \
     --inference_graph=/path/to/frozen_weights_inference_graph.pb
 
@@ -28,31 +28,89 @@ types, shapes, and semantics, as the input and output nodes of graphs produced
 by export_inference_graph.py, when run with --input_type=image_tensor.
 """
 
+import glob
+import os
 import itertools
 import tensorflow as tf
+import numpy as np
 from object_detection.inference import detection_inference
+from object_detection.core import standard_fields
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+from PIL import Image
+from io import BytesIO
 
-tf.flags.DEFINE_string('input_tfrecord_paths', None,
-                       'A comma separated list of paths to input TFRecords.')
-tf.flags.DEFINE_string('output_tfrecord_path', None,
-                       'Path to the output TFRecord.')
+tf.flags.DEFINE_string('input_tfrecord_pattern', None,
+                       'glob pattern for input tfrecords')
+tf.flags.DEFINE_string('output_images_dir', None,
+                       'Path to the output images.')
 tf.flags.DEFINE_string('inference_graph', None,
                        'Path to the inference graph with embedded weights.')
+tf.flags.DEFINE_string('label_map', None,
+                       'Path to the label map file.')
 FLAGS = tf.flags.FLAGS
+
+
+def get_image_array_from_example(tf_example):
+  encoded_jpg = tf_example.features.feature[standard_fields.TfExampleFields.image_encoded].bytes_list.value[0]
+  image = Image.open(BytesIO(encoded_jpg))
+  image_np = np.array(image)
+  return np.stack([image_np] * 3, axis=2)
+
+
+def draw_bounding_boxes_from_example(image_np, tf_example, category_index):
+  # detected bounding boxes
+  d_xmin = tf_example.features.feature[standard_fields.TfExampleFields.detection_bbox_xmin].float_list.value
+  d_xmax = tf_example.features.feature[standard_fields.TfExampleFields.detection_bbox_xmax].float_list.value
+  d_ymin = tf_example.features.feature[standard_fields.TfExampleFields.detection_bbox_ymin].float_list.value
+  d_ymax = tf_example.features.feature[standard_fields.TfExampleFields.detection_bbox_ymax].float_list.value
+  d_classes = tf_example.features.feature[standard_fields.TfExampleFields.detection_class_label].int64_list.value
+  d_scores = tf_example.features.feature[standard_fields.TfExampleFields.detection_score].float_list.value
+  vis_util.visualize_boxes_and_labels_on_image_array(
+    image_np,
+    np.stack([d_ymin, d_xmin, d_ymax, d_xmax], axis=1),
+    d_classes,
+    d_scores,
+    category_index,
+    use_normalized_coordinates=True,
+    max_boxes_to_draw=3,
+    min_score_thresh=0.0001
+  )
+
+  # ground truth bounding boxes
+  g_xmin = tf_example.features.feature[standard_fields.TfExampleFields.object_bbox_xmin].float_list.value
+  g_xmax = tf_example.features.feature[standard_fields.TfExampleFields.object_bbox_xmax].float_list.value
+  g_ymin = tf_example.features.feature[standard_fields.TfExampleFields.object_bbox_ymin].float_list.value
+  g_ymax = tf_example.features.feature[standard_fields.TfExampleFields.object_bbox_ymax].float_list.value
+  g_classes = tf_example.features.feature[standard_fields.TfExampleFields.object_class_label].int64_list.value
+  vis_util.visualize_boxes_and_labels_on_image_array(
+    image_np,
+    np.stack([g_ymin, g_xmin, g_ymax, g_xmax], axis=1),
+    g_classes,
+    None,
+    category_index,
+    use_normalized_coordinates=True,
+  )
 
 
 def main(_):
   tf.logging.set_verbosity(tf.logging.INFO)
 
-  required_flags = ['input_tfrecord_paths', 'output_tfrecord_path',
-                    'inference_graph']
+  required_flags = ['input_tfrecord_pattern', 'output_images_dir',
+                    'inference_graph', 'label_map']
   for flag_name in required_flags:
     if not getattr(FLAGS, flag_name):
       raise ValueError('Flag --{} is required'.format(flag_name))
 
+  # load the categories
+  category_index = label_map_util.create_category_index_from_labelmap(FLAGS.label_map, use_display_name=True)
+
+  # create the outputdir if it doesn't exist already
+  if not os.path.exists(FLAGS.output_images_dir):
+    os.mkdir(FLAGS.output_images_dir)
+
   with tf.Session() as sess:
-    input_tfrecord_paths = [
-        v for v in FLAGS.input_tfrecord_paths.split(',') if v]
+    input_tfrecord_paths = glob.glob(FLAGS.input_tfrecord_pattern)
     tf.logging.info('Reading input from %d files', len(input_tfrecord_paths))
     serialized_example_tensor, image_tensor = detection_inference.build_input(
         input_tfrecord_paths)
@@ -62,13 +120,21 @@ def main(_):
          image_tensor, FLAGS.inference_graph)
 
     tf.logging.info('Running inference and writing output to {}'.format(
-        FLAGS.output_tfrecord_path))
+        FLAGS.output_images_dir))
     sess.run(tf.local_variables_initializer())
     tf.train.start_queue_runners()
     try:
       for counter in itertools.count():
         tf.logging.log_every_n(tf.logging.INFO, 'Processed %d images...', 10, counter)
-        tf.logging.info(detected_scores_tensor)
+        tf_example = detection_inference.infer_detections_and_add_to_example(
+            serialized_example_tensor, detected_boxes_tensor,
+            detected_scores_tensor, detected_labels_tensor,
+            False)
+        image_np = get_image_array_from_example(tf_example)
+        draw_bounding_boxes_from_example(image_np, tf_example, category_index)
+        im = Image.fromarray(image_np)
+        pid = tf_example.features.feature[standard_fields.TfExampleFields.source_id].bytes_list.value[0].decode()
+        im.save(os.path.join(FLAGS.output_images_dir, pid + '.jpg'))
     except tf.errors.OutOfRangeError:
       tf.logging.info('Finished processing records')
 
