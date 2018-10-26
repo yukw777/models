94395d5b39d7c1defdf3316e62fb9faf8fa719ec
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-10-26 00:59:37 -0400

    always run tensorboard in the background

--- research/object_detection/pneumonia/tensorboard.sh
+++ research/object_detection/pneumonia/tensorboard.sh
@@ -3,4 +3,4 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <logdir>"
     exit 1
 fi
-docker run --runtime=nvidia --rm -p 6006:6006 -v `pwd`/"$1":/logdir yukw777/tf-models tensorboard --logdir /logdir
+docker run -d --runtime=nvidia --rm -p 6006:6006 -v `pwd`/"$1":/logdir yukw777/tf-models tensorboard --logdir /logdir
