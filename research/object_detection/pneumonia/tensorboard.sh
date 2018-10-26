5741418b58c05731828090772ebaacadb9f656fb
Peter Yu <2057325+yukw777@users.noreply.github.com>
2018-10-26 19:19:54 -0400

    add background option for tensorboard.sh

--- research/object_detection/pneumonia/tensorboard.sh
+++ research/object_detection/pneumonia/tensorboard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <logdir>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <logdir> <background>"
     exit 1
 fi
-docker run -d --runtime=nvidia --rm -p 6006:6006 -v `pwd`/"$1":/logdir yukw777/tf-models tensorboard --logdir /logdir
+docker run $2 --runtime=nvidia --rm -p 6006:6006 -v `pwd`/"$1":/logdir yukw777/tf-models tensorboard --logdir /logdir
